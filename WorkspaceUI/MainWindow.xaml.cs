11bdbca87f680a1661392bf27ffec9189e071dd3
Mauran Pavan <mauranpavan@gmail.com>
2024-03-05 13:12:15 -0500

    Add project files.

---
+++ WorkspaceUI/MainWindow.xaml.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using Path = System.IO.Path;
+
+namespace WorkspaceUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Get the path to the Application Data folder
+        public string appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+
+        // Specify the subfolder name
+        public string subfolderName = "MyWorkspace";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            workspaceOneBtn.Content = Settings.Default.workspaceOneBtn.ToString();
+            workspaceTwoBtn.Content = Settings.Default.workspaceTwoBtn.ToString();
+            workspaceThreeBtn.Content = Settings.Default.workspaceThreeBtn.ToString();
+            workspaceFourBtn.Content = Settings.Default.workspaceFourBtn.ToString();
+            workspaceFiveBtn.Content = Settings.Default.workspaceFiveBtn.ToString();
+
+            W1.Text = Settings.Default.workspaceOneBtn.ToString();
+            W2.Text = Settings.Default.workspaceTwoBtn.ToString();
+            W3.Text = Settings.Default.workspaceThreeBtn.ToString();
+            W4.Text = Settings.Default.workspaceFourBtn.ToString();
+            W5.Text = Settings.Default.workspaceFiveBtn.ToString();
+           
+
+        }
+
+        private void workspaceOneBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string batchFilePath = "C:\\Users\\mauran\\Desktop\\workflowTest\\personal_project.bat"; // Replace with the actual path to your .bat file
+
+                // Create a new process to run the batch file
+                Process process = new Process();
+                ProcessStartInfo startInfo = new ProcessStartInfo
+                {
+                    FileName = "cmd.exe", // Run the batch file via the command prompt
+                    Arguments = $"/C {batchFilePath}", // /C carries out the command specified and then terminates
+                    RedirectStandardOutput = false,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+                process.StartInfo = startInfo;
+                process.Start();
+
+                process.WaitForExit();
+            }
+            catch (Exception ex)
+            {
+                // Handle any exceptions that may occur during execution
+                MessageBox.Show($"Error-workspaceOneBtn_Click: {ex.Message}");
+            }
+
+        }
+
+        private void workspaceTwoBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Debug.WriteLine("=== workspaceTwoBtn Pressed");
+                // Combine the paths to create the full path to the subfolder
+                string subfolderPath = Path.Combine(appDataPath, subfolderName);
+                /* the workflow file name should be the selected folder directory, not the button name  */
+                string outputFileName = $@"{this.workspaceTwoBtn.Content.ToString()}-workflow.bat";
+                Debug.WriteLine("=== outputFileName: " + outputFileName);
+                string outputFilePath = Path.Combine(subfolderPath, outputFileName);
+
+                Debug.WriteLine("=== outputFilePath: " + outputFilePath);
+                string batchFilePath = outputFilePath; 
+
+                // Create a new process to run the batch file
+                Process process = new Process();
+                ProcessStartInfo startInfo = new ProcessStartInfo
+                {
+                    FileName = "cmd.exe", // Run the batch file via the command prompt
+                    Arguments = $"/C {batchFilePath}", // /C carries out the command specified and then terminates
+                    RedirectStandardOutput = false,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+                process.StartInfo = startInfo;
+                process.Start();
+
+                process.WaitForExit();
+            }
+            catch (Exception ex)
+            {
+                // Handle any exceptions that may occur during execution
+                MessageBox.Show($"Error-workspaceTwoBtn_Click: {ex.Message}");
+            }
+        }
+
+        private void workspaceThreeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Debug.WriteLine("=== workspaceThreeBtn Pressed");
+                string subfolderPath = Path.Combine(appDataPath, subfolderName);
+
+
+                //batch file
+                QuickOpenGoogleDrive quickOpenGoogleDrive = new QuickOpenGoogleDrive();
+                string outputFilePath = quickOpenGoogleDrive.QuickOpen($@"{this.workspaceThreeBtn.Content.ToString()}", "Lecture");
+
+                Debug.WriteLine("=== outputFilePath: " + outputFilePath);
+                string batchFilePath = outputFilePath;
+
+
+
+                // Create a new process to run the batch file
+                Process process = new Process();
+                ProcessStartInfo startInfo = new ProcessStartInfo
+                {
+                    FileName = "cmd.exe", // Run the batch file via the command prompt
+                    Arguments = $"/C {batchFilePath}", // /C carries out the command specified and then terminates
+                    RedirectStandardOutput = false,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+                process.StartInfo = startInfo;
+                process.Start();
+
+                process.WaitForExit();
+            }
+            catch (Exception ex)
+            {
+                // Handle any exceptions that may occur during execution
+                MessageBox.Show($"Error-workspaceThreeBtn_Click: {ex.Message}");
+            }
+        }
+
+        private void workspaceFourBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Debug.WriteLine("=== workspaceFourBtn Pressed");
+                // Combine the paths to create the full path to the subfolder
+                string subfolderPath = Path.Combine(appDataPath, subfolderName);
+
+                /* the workflow file name should be the selected folder directory, not the button name  */
+
+                //batch file
+                QuickOpenGoogleDrive quickOpenGoogleDrive = new QuickOpenGoogleDrive();
+                string outputFilePath = quickOpenGoogleDrive.QuickOpen($@"{this.workspaceFourBtn.Content.ToString()}", "Lecture");
+
+                //string outputFileName = $@"{this.workspaceFourBtn.Content.ToString()}-workflow.bat";
+                //Debug.WriteLine("=== outputFileName: " + outputFileName);
+                //string outputFilePath = Path.Combine(subfolderPath, outputFileName);
+
+                Debug.WriteLine("=== outputFilePath: " + outputFilePath);
+                string batchFilePath = outputFilePath;
+
+      
+
+                // Create a new process to run the batch file
+                Process process = new Process();
+                ProcessStartInfo startInfo = new ProcessStartInfo
+                {
+                    FileName = "cmd.exe", // Run the batch file via the command prompt
+                    Arguments = $"/C {batchFilePath}", // /C carries out the command specified and then terminates
+                    RedirectStandardOutput = false,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+                process.StartInfo = startInfo;
+                process.Start();
+
+                process.WaitForExit();
+            }
+            catch (Exception ex)
+            {
+                // Handle any exceptions that may occur during execution
+                MessageBox.Show($"Error-workspaceFourBtn_Click: {ex.Message}");
+            }
+        }
+
+        private void workspaceFiveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Debug.WriteLine("=== workspaceFiveBtn Pressed");
+                string subfolderPath = Path.Combine(appDataPath, subfolderName);
+
+
+                //batch file
+                QuickOpenGoogleDrive quickOpenGoogleDrive = new QuickOpenGoogleDrive();
+                string outputFilePath = quickOpenGoogleDrive.QuickOpen($@"{this.workspaceFiveBtn.Content.ToString()}", "Lecture");
+
+                Debug.WriteLine("=== outputFilePath: " + outputFilePath);
+                string batchFilePath = outputFilePath;
+
+
+
+                // Create a new process to run the batch file
+                Process process = new Process();
+                ProcessStartInfo startInfo = new ProcessStartInfo
+                {
+                    FileName = "cmd.exe", // Run the batch file via the command prompt
+                    Arguments = $"/C {batchFilePath}", // /C carries out the command specified and then terminates
+                    RedirectStandardOutput = false,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+                process.StartInfo = startInfo;
+                process.Start();
+
+                process.WaitForExit();
+            }
+            catch (Exception ex)
+            {
+                // Handle any exceptions that may occur during execution
+                MessageBox.Show($"Error-workspaceFiveBtn_Click: {ex.Message}");
+            }
+        }
+
+        private void saveBtn_Click(object sender, RoutedEventArgs e)
+        {       
+
+            workspaceOneBtn.Content = W1.Text;
+            workspaceTwoBtn.Content = W2.Text;
+            workspaceThreeBtn.Content = W3.Text;
+            workspaceFourBtn.Content = W4.Text;
+            workspaceFiveBtn.Content = W5.Text;
+
+            // Update the setting when the button is clicked
+            Settings.Default.workspaceOneBtn = W1.Text;
+            Settings.Default.workspaceTwoBtn = W2.Text;
+            Settings.Default.workspaceThreeBtn = W3.Text;
+            Settings.Default.workspaceFourBtn = W4.Text;
+            Settings.Default.workspaceFiveBtn = W5.Text;
+
+            // Save the changes to persist them
+            Settings.Default.Save();
+
+            WorkspaceTabControl.SelectedIndex = 0;
+        }
+
+        private void cancelBtn_Click(object sender, RoutedEventArgs e)
+        {
+            WorkspaceTabControl.SelectedIndex = 0;
+        }
+    }
+}
---
+++ WorkspaceUI/QuickOpenGoogleDrive.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkspaceUI
+{
+using System;
+using System.IO;
+using System.Collections.Generic;
+    using System.Diagnostics;
+
+    public class QuickOpenGoogleDrive
+    {
+        public QuickOpenGoogleDrive() { }
+     
+        public string QuickOpen(string dirName, string folderName)
+        {
+            // i have to give explicit abs path for the output file to be written in for some reason
+            Debug.WriteLine("Starting");
+            //string outputFileName = $@"C:\Users\mauran\Documents\WorkspaceUI\WorkspaceUI\script\{dirName}-workflow.bat";
+            string outputFileName = $@"{dirName}-workflow.bat";
+
+            string directoryPath = $@"G:\My Drive\{dirName}\{folderName}";
+            string openNewChromePrefixCMD = "start chrome /new-window ";
+            string openLinkPrefixCMD = "start \"\" ";
+            bool startFlag = true;
+            List<string> allFilesList = new List<string>();
+            //application data folder
+            try
+            {
+                //Part 1
+                // Get the path to the Application Data folder
+                string appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+
+                // Specify the subfolder name
+                string subfolderName = "MyWorkspace"; // Change this to your actual application name
+
+                // Combine the paths to create the full path to the subfolder
+                string subfolderPath = Path.Combine(appDataPath, subfolderName);
+
+                // Check if the subfolder exists, and create it if not
+                if (!Directory.Exists(subfolderPath))
+                {
+                    Directory.CreateDirectory(subfolderPath);
+                    Debug.WriteLine($"Subfolder '{subfolderName}' created in the Application Data folder.");
+                }
+                else
+                {
+                    Debug.WriteLine($"Subfolder '{subfolderName}' already exists in the Application Data folder.");
+                }
+
+                //Part 2
+                //Walk through the directory and its subdirectories
+                foreach (string filePath in Directory.EnumerateFiles(directoryPath, "*", SearchOption.AllDirectories))
+                {
+                    allFilesList.Add(filePath);
+                }
+
+                foreach (string filePath in allFilesList)
+                {
+                    Debug.WriteLine(filePath);
+                }
+
+                //Part 3
+                //Open the output file in write mode
+                Debug.WriteLine("Writing to output file");
+                string outputFilePath = Path.Combine(subfolderPath, outputFileName);
+                using (StreamWriter outputFile = new StreamWriter(outputFilePath, true, Encoding.UTF8))
+                //using (StreamWriter outputFile = new StreamWriter(Path.Combine(appDataPath, subfolderName)))
+                {
+                    // Write the list of files to the text file
+                    outputFile.WriteLine("@echo off");
+                    Debug.WriteLine($"Sample file '{outputFilePath}' created in the subfolder.");
+
+                    foreach (string filename in allFilesList)
+                    {
+                        // If this is the first file, then open it in a new Chrome Window
+                        if (startFlag)
+                        {
+                            outputFile.WriteLine(openNewChromePrefixCMD + "\"" + filename + "\"" + "\n");
+                            startFlag = false;
+                        }
+                        else
+                        {
+                            outputFile.WriteLine(openLinkPrefixCMD + "\"" + filename + "\"" + "\n");
+                        }
+                    }
+
+                    outputFile.WriteLine("exit");
+                }
+
+                Debug.WriteLine($"File list has been written to {outputFileName}");
+                return outputFilePath;
+
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Error: {ex.Message}");
+                return String.Empty;
+            }
+
+           
+        }
+    }
+
+}
