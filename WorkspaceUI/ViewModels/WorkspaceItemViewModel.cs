4ace7a514dafe58cd46d150aa715403b550f442e
Mauran Pavan <mauranpavan@gmail.com>
2024-08-30 11:44:04 -0400

    added a WorkspaceItemViewModel to use with a Listbox in UI

---
+++ WorkspaceUI/ViewModels/WorkspaceItemViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkspaceUI.ViewModels
+{
+    class WorkspaceItemViewModel : INotifyPropertyChanged
+    {
+        // ObservableCollection to hold the list of items
+        private ObservableCollection<string> _items = new ObservableCollection<string>{};
+        public ObservableCollection<string> Items
+        {
+            get => _items;
+            set { _items = value; OnPropertyChanged(); }
+        }
+
+        public WorkspaceItemViewModel()
+        {
+            Items = new ObservableCollection<string>
+            {
+                "Workspace Item 1",
+                "Workspace Item 2",
+                "Workspace Item 3"
+            };
+        }
+
+        // Method to add an item
+        public void AddItem(string item)
+        {
+            Items.Add(item);
+        }
+
+        // Method to remove an item
+        public void RemoveItem(string item)
+        { 
+            Items.Remove(item);
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string? name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
+
+
