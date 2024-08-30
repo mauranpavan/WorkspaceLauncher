8625c4168537e69ce53ebdf832a2a4bbc5b20f56
Mauran Pavan <mauranpavan@gmail.com>
2024-08-30 11:35:21 -0400

    redesigned main window

--- WorkspaceUI/MainWindow.xaml.cs
+++ WorkspaceUI/MainWindow.xaml.cs
@@ -16,6 +16,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO;
 using Path = System.IO.Path;
+using WorkspaceUI.UserControls;
 
 namespace WorkspaceUI
 {
@@ -39,20 +40,73 @@ namespace WorkspaceUI
             workspaceFourBtn.Content = Settings.Default.workspaceFourBtn.ToString();
             workspaceFiveBtn.Content = Settings.Default.workspaceFiveBtn.ToString();
 
-            W1.Text = Settings.Default.workspaceOneBtn.ToString();
-            W2.Text = Settings.Default.workspaceTwoBtn.ToString();
-            W3.Text = Settings.Default.workspaceThreeBtn.ToString();
-            W4.Text = Settings.Default.workspaceFourBtn.ToString();
-            W5.Text = Settings.Default.workspaceFiveBtn.ToString();
-           
+            //SettingsWindow settingsWindow = new SettingsWindow();
+            //TextBox W1 = (TextBox)settingsWindow.FindName("workspaceOneBtn");
+            //TextBox W2 = (TextBox)settingsWindow.FindName("workspaceTwoBtn");
+            //TextBox W3 = (TextBox)settingsWindow.FindName("workspaceThreeBtn");
+            //TextBox W4 = (TextBox)settingsWindow.FindName("workspaceFourBtn");
+            //TextBox W5 = (TextBox)settingsWindow.FindName("workspaceFiveBtn");
+
+            //ActionConfigWindow actionConfigWindow = new ActionConfigWindow();
+            //TextBox W1 = (TextBox)actionConfigWindow.FindName("workspaceOneBtn");
+            //TextBox W2 = (TextBox)actionConfigWindow.FindName("workspaceTwoBtn");
+            //TextBox W3 = (TextBox)actionConfigWindow.FindName("workspaceThreeBtn");
+            //TextBox W4 = (TextBox)actionConfigWindow.FindName("workspaceFourBtn");
+            //TextBox W5 = (TextBox)actionConfigWindow.FindName("workspaceFiveBtn");
+
+            //W1.Text = Settings.Default.workspaceOneBtn.ToString();
+            //W2.Text = Settings.Default.workspaceTwoBtn.ToString();
+            //W3.Text = Settings.Default.workspaceThreeBtn.ToString();
+            //W4.Text = Settings.Default.workspaceFourBtn.ToString();
+            //W5.Text = Settings.Default.workspaceFiveBtn.ToString();
+
 
         }
+
+        //private void workspaceOneBtn_Click(object sender, RoutedEventArgs e)
+        //{
+        //    try
+        //    {
+        //        string batchFilePath = "C:\\Users\\mauran\\Desktop\\workflowTest\\personal_project.bat"; // Replace with the actual path to your .bat file
+
+        //        // Create a new process to run the batch file
+        //        Process process = new Process();
+        //        ProcessStartInfo startInfo = new ProcessStartInfo
+        //        {
+        //            FileName = "cmd.exe", // Run the batch file via the command prompt
+        //            Arguments = $"/C {batchFilePath}", // /C carries out the command specified and then terminates
+        //            RedirectStandardOutput = false,
+        //            UseShellExecute = false,
+        //            CreateNoWindow = true
+        //        };
+        //        process.StartInfo = startInfo;
+        //        process.Start();
+
+        //        process.WaitForExit();
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        // Handle any exceptions that may occur during execution
+        //        MessageBox.Show($"Error-workspaceOneBtn_Click: {ex.Message}");
+        //    }
+
+        //}
 
         private void workspaceOneBtn_Click(object sender, RoutedEventArgs e)
         {
             try
             {
-                string batchFilePath = "C:\\Users\\mauran\\Desktop\\workflowTest\\personal_project.bat"; // Replace with the actual path to your .bat file
+                Debug.WriteLine("=== workspaceOneBtn Pressed");
+                // Combine the paths to create the full path to the subfolder
+                string subfolderPath = Path.Combine(appDataPath, subfolderName);
+
+                string outputFileName = "W1-workflow.bat";
+
+                Debug.WriteLine("=== outputFileName: " + outputFileName);
+                string outputFilePath = Path.Combine(subfolderPath, outputFileName);
+
+                Debug.WriteLine($"=== outputFilePath: {outputFilePath}");
+                string batchFilePath = outputFilePath;
 
                 // Create a new process to run the batch file
                 Process process = new Process();
@@ -84,12 +138,15 @@ namespace WorkspaceUI
                 Debug.WriteLine("=== workspaceTwoBtn Pressed");
                 // Combine the paths to create the full path to the subfolder
                 string subfolderPath = Path.Combine(appDataPath, subfolderName);
+
                 /* the workflow file name should be the selected folder directory, not the button name  */
-                string outputFileName = $@"{this.workspaceTwoBtn.Content.ToString()}-workflow.bat";
+                //string outputFileName = $@"{this.workspaceTwoBtn.Content.ToString()}-workflow.bat";
+                string outputFileName = "W2-workflow.bat";
+
                 Debug.WriteLine("=== outputFileName: " + outputFileName);
                 string outputFilePath = Path.Combine(subfolderPath, outputFileName);
 
-                Debug.WriteLine("=== outputFilePath: " + outputFilePath);
+                Debug.WriteLine($"=== outputFilePath: {outputFilePath}");
                 string batchFilePath = outputFilePath; 
 
                 // Create a new process to run the batch file
@@ -120,11 +177,16 @@ namespace WorkspaceUI
             {
                 Debug.WriteLine("=== workspaceThreeBtn Pressed");
                 string subfolderPath = Path.Combine(appDataPath, subfolderName);
+                string outputFilePath = $@"{this.workspaceThreeBtn.Content.ToString()}-workflow.bat";
 
 
                 //batch file
+                if (File.Exists(outputFilePath))
+                {
+                    File.Delete(outputFilePath);
+                }
                 QuickOpenGoogleDrive quickOpenGoogleDrive = new QuickOpenGoogleDrive();
-                string outputFilePath = quickOpenGoogleDrive.QuickOpen($@"{this.workspaceThreeBtn.Content.ToString()}", "Lecture");
+                outputFilePath = quickOpenGoogleDrive.QuickOpen($@"{this.workspaceThreeBtn.Content.ToString()}", "Lecture");
 
                 Debug.WriteLine("=== outputFilePath: " + outputFilePath);
                 string batchFilePath = outputFilePath;
@@ -213,8 +275,6 @@ namespace WorkspaceUI
                 Debug.WriteLine("=== outputFilePath: " + outputFilePath);
                 string batchFilePath = outputFilePath;
 
-
-
                 // Create a new process to run the batch file
                 Process process = new Process();
                 ProcessStartInfo startInfo = new ProcessStartInfo
@@ -237,31 +297,12 @@ namespace WorkspaceUI
             }
         }
 
-        private void saveBtn_Click(object sender, RoutedEventArgs e)
-        {       
-
-            workspaceOneBtn.Content = W1.Text;
-            workspaceTwoBtn.Content = W2.Text;
-            workspaceThreeBtn.Content = W3.Text;
-            workspaceFourBtn.Content = W4.Text;
-            workspaceFiveBtn.Content = W5.Text;
-
-            // Update the setting when the button is clicked
-            Settings.Default.workspaceOneBtn = W1.Text;
-            Settings.Default.workspaceTwoBtn = W2.Text;
-            Settings.Default.workspaceThreeBtn = W3.Text;
-            Settings.Default.workspaceFourBtn = W4.Text;
-            Settings.Default.workspaceFiveBtn = W5.Text;
-
-            // Save the changes to persist them
-            Settings.Default.Save();
-
-            WorkspaceTabControl.SelectedIndex = 0;
-        }
-
-        private void cancelBtn_Click(object sender, RoutedEventArgs e)
+        private void openSettings(object sender, RoutedEventArgs e)
         {
-            WorkspaceTabControl.SelectedIndex = 0;
+            //SettingsWindow settingsWindow = new SettingsWindow();
+            //settingsWindow.Show();
+            ActionConfigWindow actionConfigWindow = new ActionConfigWindow();
+            actionConfigWindow.Show();
         }
     }
 }
