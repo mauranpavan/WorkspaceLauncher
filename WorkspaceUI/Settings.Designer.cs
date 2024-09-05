97b7a855b7d58a4bdfc0502ab6c5b65eadbe3e98
Mauran Pavan <mauranpavan@gmail.com>
2024-09-05 14:03:48 -0400

    updated application setings for W3, W4 and W5

--- WorkspaceUI/Settings.Designer.cs
+++ WorkspaceUI/Settings.Designer.cs
@@ -85,7 +85,7 @@ namespace WorkspaceUI {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("W1 App")]
+        [global::System.Configuration.DefaultSettingValueAttribute("W1 App Name")]
         public string W1SelectedAppName {
             get {
                 return ((string)(this["W1SelectedAppName"]));
@@ -97,7 +97,7 @@ namespace WorkspaceUI {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("W1 File")]
+        [global::System.Configuration.DefaultSettingValueAttribute("W1 File Name")]
         public string W1SelectedFileName {
             get {
                 return ((string)(this["W1SelectedFileName"]));
@@ -133,7 +133,7 @@ namespace WorkspaceUI {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("W2 App")]
+        [global::System.Configuration.DefaultSettingValueAttribute("W2 App Name")]
         public string W2SelectedAppName {
             get {
                 return ((string)(this["W2SelectedAppName"]));
@@ -145,7 +145,7 @@ namespace WorkspaceUI {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("W2 File")]
+        [global::System.Configuration.DefaultSettingValueAttribute("W2 File Name")]
         public string W2SelectedFileName {
             get {
                 return ((string)(this["W2SelectedFileName"]));
@@ -181,17 +181,6 @@ namespace WorkspaceUI {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        public global::System.Collections.Specialized.StringCollection W1StringPairsJson {
-            get {
-                return ((global::System.Collections.Specialized.StringCollection)(this["W1StringPairsJson"]));
-            }
-            set {
-                this["W1StringPairsJson"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("")]
         public string W1WorkspaceItemsJsonString {
             get {
@@ -211,6 +200,186 @@ namespace WorkspaceUI {
             }
             set {
                 this["W2WorkspaceItemsJsonString"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string W3WorkspaceItemsJsonString {
+            get {
+                return ((string)(this["W3WorkspaceItemsJsonString"]));
+            }
+            set {
+                this["W3WorkspaceItemsJsonString"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string W4WorkspaceItemsJsonString {
+            get {
+                return ((string)(this["W4WorkspaceItemsJsonString"]));
+            }
+            set {
+                this["W4WorkspaceItemsJsonString"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string W5WorkspaceItemsJsonString {
+            get {
+                return ((string)(this["W5WorkspaceItemsJsonString"]));
+            }
+            set {
+                this["W5WorkspaceItemsJsonString"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("W3 App Name")]
+        public string W3SelectedAppName {
+            get {
+                return ((string)(this["W3SelectedAppName"]));
+            }
+            set {
+                this["W3SelectedAppName"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("W3 App Path")]
+        public string W3SelectedAppPath {
+            get {
+                return ((string)(this["W3SelectedAppPath"]));
+            }
+            set {
+                this["W3SelectedAppPath"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("W3 File Name")]
+        public string W3SelectedFileName {
+            get {
+                return ((string)(this["W3SelectedFileName"]));
+            }
+            set {
+                this["W3SelectedFileName"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("W3 File Path")]
+        public string W3SelectedFilePath {
+            get {
+                return ((string)(this["W3SelectedFilePath"]));
+            }
+            set {
+                this["W3SelectedFilePath"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("W4 App Name")]
+        public string W4SelectedAppName {
+            get {
+                return ((string)(this["W4SelectedAppName"]));
+            }
+            set {
+                this["W4SelectedAppName"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("W4 App Path")]
+        public string W4SelectedAppPath {
+            get {
+                return ((string)(this["W4SelectedAppPath"]));
+            }
+            set {
+                this["W4SelectedAppPath"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("W4 File Name")]
+        public string W4SelectedFileName {
+            get {
+                return ((string)(this["W4SelectedFileName"]));
+            }
+            set {
+                this["W4SelectedFileName"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("W4 File Path")]
+        public string W4SelectedFilePath {
+            get {
+                return ((string)(this["W4SelectedFilePath"]));
+            }
+            set {
+                this["W4SelectedFilePath"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("W5 App Name")]
+        public string W5SelectedAppName {
+            get {
+                return ((string)(this["W5SelectedAppName"]));
+            }
+            set {
+                this["W5SelectedAppName"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("W5 App Path")]
+        public string W5SelectedAppPath {
+            get {
+                return ((string)(this["W5SelectedAppPath"]));
+            }
+            set {
+                this["W5SelectedAppPath"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("W5 File Name")]
+        public string W5SelectedFileName {
+            get {
+                return ((string)(this["W5SelectedFileName"]));
+            }
+            set {
+                this["W5SelectedFileName"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("W5 File Path")]
+        public string W5SelectedFilePath {
+            get {
+                return ((string)(this["W5SelectedFilePath"]));
+            }
+            set {
+                this["W5SelectedFilePath"] = value;
             }
         }
     }
