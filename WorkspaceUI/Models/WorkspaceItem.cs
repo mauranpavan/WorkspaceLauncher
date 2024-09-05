462870c0284eda1994577bc0d57656dc98a57330
Mauran Pavan <mauranpavan@gmail.com>
2024-09-05 00:05:40 -0400

    implemented WorkspaceItem core model class

---
+++ WorkspaceUI/Models/WorkspaceItem.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkspaceUI.Models
+{
+    internal class WorkspaceItem
+    {
+        //public string? ApplicationName { get; set; }
+        public string? ApplicationPath { get; set; }
+        public string? FileName { get; set; }
+        public string? FilePath { get; set; }
+
+        private string _applicationName;
+
+        public string ApplicationName
+        {
+            get => _applicationName;
+            set
+            {
+                if (_applicationName != value)
+                {
+                    _applicationName = value;
+                    OnPropertyChanged(nameof(ApplicationName)); // Ensure that the correct property name is passed
+                }
+            }
+        }
+
+        public WorkspaceItem() { }
+
+        public WorkspaceItem(int workspaceIndex)
+        {
+            ApplicationName = "Workspace Application " + workspaceIndex;
+            ApplicationPath = "Workspace Application Path " + workspaceIndex;
+            FileName = "Workspace File " + workspaceIndex;
+            FilePath = "Workspace File Path " + workspaceIndex;
+        }
+
+        public WorkspaceItem(string? applicationName, string? applicationPath, string? fileName, string? filePath)
+        {
+            this.ApplicationName = applicationName;
+            this.ApplicationPath = applicationPath;
+            this.FileName = fileName;
+            this.FilePath = filePath;
+        }
+
+        public override string ToString()
+        {
+
+            return String.Format("{0}==> {1}{2}{3}==> {4}", ApplicationName, ApplicationPath, Environment.NewLine,
+                FileName, FilePath);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+}
