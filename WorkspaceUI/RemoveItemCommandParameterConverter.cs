98cdabd36dc78a154ec9dcddf5b82d3f849aabd1
Mauran Pavan <mauranpavan@gmail.com>
2024-09-05 00:15:42 -0400

    implemented class to rely command such as to remove items

---
+++ WorkspaceUI/RemoveItemCommandParameterConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using WorkspaceUI.Models;
+
+namespace WorkspaceUI
+{
+    internal class RemoveItemCommandParameterConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var collection = values[0] as ObservableCollection<WorkspaceItem>;
+            var item = values[1] as WorkspaceItem;
+
+            return (collection, item);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
